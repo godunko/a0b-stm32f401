ef7bdce0248994a150af4b5ffb000acff41f44ed
Vadim Godunko <vgodunko@gmail.com>
2024-09-23 20:18:05 +0400

    Initial code.

---
+++ source/a0b-stm32f401.ads
@@ -0,0 +1,15 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+pragma Restrictions (No_Elaboration_Code);
+
+with A0B.ARMv7M;
+
+package A0B.STM32F401
+  with Preelaborate
+is
+
+end A0B.STM32F401;
